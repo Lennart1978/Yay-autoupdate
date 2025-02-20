bf5cd422314b22c288b4b2d7e79af93575b91eb8
Lennart Martens <monkeynator78@gmail.com>
2025-02-20 10:27:19 +0100

    Update yay-update.sh

--- yay-update.sh
+++ yay-update.sh
@@ -20,23 +20,23 @@ sleep 5
 ID=$(notify-send --print-id -t 1000 -u normal -a Yay --hint=string:sound-name:service-login 'Yay Update' 'Starting system update ...')
 
 # Log mit Header
-echo -e "\n$(date): === Update gestartet ===" >> "$LOG"
+echo -e "\n$(date): === Update started ===" >> "$LOG"
 
 # Systemupdate
 if ! yay --noconfirm --sudoloop --needed 2>&1 | tee -a "$LOG"; then
     beep -f 2000 
-    notify-send -r "$ID" -t 5000 -u critical -a Yay -i /usr/share/icons/Adwaita/symbolic/status/computer-fail-symbolic.svg --hint=string:sound-name:dialog-error 'Yay Update' 'Fehler beim Update! Siehe Log.'
+    notify-send -r "$ID" -t 5000 -u critical -a Yay -i /usr/share/icons/Adwaita/symbolic/status/computer-fail-symbolic.svg --hint=string:sound-name:dialog-error 'Yay Update' 'Error ! see log file !'
     exit 1
 fi
 
 # Keine Updates nötig
-if grep -q "es gibt nichts zu tun" "$LOG"; then
-    notify-send -r "$ID" -t 3000 -u normal -a Yay --hint=string:sound-name:dialog-information 'Yay Update' 'System ist aktuell !'
+if grep -q "there is nothing to do" "$LOG"; then
+    notify-send -r "$ID" -t 3000 -u normal -a Yay --hint=string:sound-name:dialog-information 'Yay Update' 'System is uptodate !'
     exit 0
 fi
 
 # Cache säubern
-echo "$(date): Bereinige Cache..." >> "$LOG"
+echo "$(date): cleaning cache..." >> "$LOG"
 yay -Scc --noconfirm 2>&1 | tee -a "$LOG"
 sudo rm -fv /var/cache/pacman/pkg/* >> "$LOG"
 
@@ -44,7 +44,7 @@ sudo rm -fv /var/cache/pacman/pkg/* >> "$LOG"
 sudo logrotate -f -v /etc/logrotate.d/pacman 2>&1 | tee -a "$LOG"
 
 # Erfolgsbenachrichtigung
-notify-send -r "$ID" -t 3000 -u normal -a Yay --hint=string:sound-name:complete 'Yay Update' 'Update erfolgreich !'
+notify-send -r "$ID" -t 3000 -u normal -a Yay --hint=string:sound-name:complete 'Yay Update' 'Update successful !'
 
 # Die aktualisierten Pakete in einem Zenity - Info Dialog anzeigen
 show-updated.sh
