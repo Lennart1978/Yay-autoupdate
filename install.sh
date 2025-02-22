6a1b0382ef50713a11e950230cf7650b86903952
Lennart Martens <monkeynator78@gmail.com>
2025-02-22 18:07:34 +0100

    Update to version 1.1
    Added "logrotate" as dependency.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 echo "Yay-autoupdate version 1.0 - installation:"
 
 # Array containing the packages to check
-packages=("zenity" "libnotify" "beep")
+packages=("zenity" "libnotify" "beep" "logrotate")
 
 # Array to store missing packages
 missing_packages=()
